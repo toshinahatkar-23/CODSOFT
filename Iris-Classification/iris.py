a112806b34ed46dadd6eb828deb62e569105a819
toshinahatkar-23 <157872776+toshinahatkar-23@users.noreply.github.com>
2026-04-08 21:53:06 +0530

    Added Iris-Classification project files

---
+++ Iris-Classification/iris.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data=load_iris()
+print(data.data[:5])
+
+X=data.data
+y=data.target
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+print("Training size:",len(X_train))
+print("Testing size:",len(X_test))
+
+model=LogisticRegression()
+model.fit(X_train,y_train)
+
+predictions=model.predict(X_test)
+
+print("Accuracy:",accuracy_score(y_test,predictions))

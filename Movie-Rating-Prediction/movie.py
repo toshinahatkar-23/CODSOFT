df1b2f57419bcd5cb5a92e07704ef6c11a74ecdd
toshinahatkar-23 <157872776+toshinahatkar-23@users.noreply.github.com>
2026-04-11 01:45:20 +0530

    Implemented Movie Rating Prediction using Random Forest with data preprocessing, feature engineering, evaluation metrics (R2, MSE), and visualization

--- Movie-Rating-Prediction/movie.py
+++ Movie-Rating-Prediction/movie.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
 data=pd.read_csv("movies.csv",encoding='latin1')
 print(data.head())
 
@@ -32,7 +39,7 @@ data['Actor 3']=data['Actor 3'].fillna('unknown')
 print("Cleaned Data:")
 print(data.head())
 
-from sklearn.preprocessing import LabelEncoder
+
 
 le_genre = LabelEncoder()
 le_director = LabelEncoder()
@@ -51,18 +58,39 @@ print("Encoded Data:")
 print(data.head())
 
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
 X=data[['Year','Duration','Genre','Director','Actor 1','Actor 2','Actor 3']]
 Y=data['Rating']
 
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
-model=LinearRegression()
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2, random_state=42)
+model = RandomForestRegressor()
 model.fit(X_train,Y_train)
 
 predictions=model.predict(X_test)
-
+print("\n--- Model Performance ---")
+print("R2 SCORE:",r2_score(Y_test,predictions))
 print("MSE:",mean_squared_error(Y_test,predictions))
+
+
+print("\n--- New Movie Predictions ---")
+new_movies = pd.DataFrame([
+    [2020, 120, 1, 500, 1000, 1500, 2000],
+    [2015, 150, 2, 300, 800, 1200, 1600],
+    [2010, 100, 3, 200, 600, 900, 1100]
+],
+columns=['Year','Duration','Genre','Director','Actor 1','Actor 2','Actor 3'])
+new_predictions = model.predict(new_movies)
+for i, pred in enumerate(new_predictions[:5]):
+    print(f"Movie {i+1} Predicted Rating: {round(pred,2)}")
+
+predicted_rating = model.predict(new_movies)
+print("\n--- Feature Importance ---")
+importance = pd.Series(model.feature_importances_, index=X.columns)
+importance = importance.sort_values(ascending=False)
+print(importance)
+
+plt.scatter(Y_test, predictions)
+plt.xlabel("Actual Rating")
+plt.ylabel("Predicted Rating")
+plt.title("Actual vs Predicted Ratings")
+plt.show()
  

c0b9951b50a6e71ce47bc27980e8c35010d304c4
toshinahatkar-23 <157872776+toshinahatkar-23@users.noreply.github.com>
2026-04-11 02:46:51 +0530

    Completed Sales Prediction project with regression model, evaluation metrics, and visualization

--- Sales-Prediction/sales.py
+++ Sales-Prediction/sales.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-data=pd.read_csv("advertising.csv")
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 
+data=pd.read_csv("advertising.csv")
+print("\n--- Dataset Preview ---")
 print(data.head())
 
 X=data[['TV','Radio','Newspaper']]
@@ -19,6 +22,27 @@ print("Testing size:",len(X_test))
 
 model=LinearRegression()
 model.fit(X_train,y_train)
-
 predictions=model.predict(X_test)
-print("MSE:",mean_squared_error(y_test,predictions))
+
+print("\n--- New Prediction ---")
+new_data = pd.DataFrame([[200, 40, 50]],
+columns=['TV','Radio','Newspaper'])
+
+pred = model.predict(new_data)
+
+print("Predicted Sales:", round(pred[0],2))
+
+importance = pd.Series(model.coef_, index=X.columns)
+
+print("\n--- Feature Importance ---")
+print(importance)
+
+print("\n--- Model Performance ---")
+print("R2 Score:", r2_score(y_test, predictions))
+print("MSE:", mean_squared_error(y_test, predictions))
+
+plt.scatter(y_test, predictions)
+plt.xlabel("Actual Sales")
+plt.ylabel("Predicted Sales")
+plt.title("Actual vs Predicted Sales")
+plt.show()

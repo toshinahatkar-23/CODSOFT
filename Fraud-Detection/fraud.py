cd30c06e262748973910178a03a465c736a33006
toshinahatkar-23 <157872776+toshinahatkar-23@users.noreply.github.com>
2026-04-11 03:03:18 +0530

    Enhanced fraud detection model with scaling, class imbalance handling, confusion matrix, and performance evaluation

--- Fraud-Detection/fraud.py
+++ Fraud-Detection/fraud.py
@@ -3,6 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
 data=pd.read_csv("creditcard.csv")
 print(data.head())
 
@@ -12,15 +15,31 @@ y=data['Class']
 
 print(X.head())
 print(y.head())
-
 print(y.value_counts())
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 print("Training size:",len(X_train))
 print("Testing size:",len(X_test))
 
-model=LogisticRegression(max_iter=5000)
+
+model = LogisticRegression(max_iter=5000, class_weight='balanced')
 model.fit(X_train,y_train)
 
-predictions=model.predict(X_test)
+print("\n--- Sample Prediction ---")
+sample = X_test[0].reshape(1, -1)
+pred = model.predict(sample)
+print("Prediction:", "Fraud" if pred[0]==1 else "Not Fraud")
 
+
+predictions=model.predict(X_test)
+print("\n--- Confusion Matrix ---")
+print(confusion_matrix(y_test, predictions))
+
+print("\n--- Accuracy ---")
+print("Accuracy:", accuracy_score(y_test, predictions))
+
+print("\n--- Classification Report ---")
 print(classification_report(y_test,predictions))
